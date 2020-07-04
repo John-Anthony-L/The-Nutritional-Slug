95216f1c1c7737f68b57e9f64d8fa06b669435bb
Vidisha Nevatia <vidishanevatia@gmail.com>
2020-07-05 04:03:33 +0530

    Updated project with UI

---
+++ SlugNutrition/Product.swift
@@ -0,0 +1,30 @@
+//
+//  Product.swift
+//  SlugNutrition
+//
+//  THIS APP IS BASED ON THE CODE PROVIDED BY BRIAN ADVENT
+// https://www.youtube.com/watch?v=tdxKIPpPDAI
+//
+//  Created by Roberto Oregon on 07.03.20.
+//  Copyright © 2020 Roberto Oregon. All rights reserved.
+//
+
+import Foundation
+/*
+struct ProductResponse:Decodable {
+    var products:[ProductDetail]
+}
+
+struct ProductDetail:Decodable {
+    var id:String
+    var title:String
+}
+*/
+
+struct ProductResponse:Codable{
+    struct ProductDetail: Codable{
+        let id: Int
+        let title:String
+    }
+    let products:[ProductDetail]
+}
