9181881cfa0e7e23a949bf5b37ad5d68c0bd6ed5
raoregon <55852350+raoregon@users.noreply.github.com>
2020-07-05 03:28:13 -0700

    Add files via upload
    -Reuploaded codebase with UI and Search function properly linked together
    -Changed title screen to say "Let's" instead of "Let Us", just easier to say imo, we can change it back
    -To update codebase: 
    1) use git to create a local copy of the codebase to your computer and link it to the repository
    2) send your commits using git 
    OR 
    1) download the codebase 
    2) after making changes, update the changed files 
    3) you may also need to overwrite the main.storyboard file 
    4) you will also need to overwrite the SlugNutrition.xcodeproj file ( just upload as a folder, not a zip)
    IF SOMETHING GOES BAD: just let me(Roberto) know, send me your edits, i'll either roll back the codebase and add your changes or whatever else to get the code back to working ;D

---
+++ SlugNutrition/SlugNutrition/ProductRequest.swift
@@ -0,0 +1,51 @@
+//
+//  ProductRequest.swift
+//  SlugNutrition
+//
+//  THIS .SWIFT FILE IS BASED ON THE CODE PROVIDED BY BRIAN ADVENT
+// https://www.youtube.com/watch?v=tdxKIPpPDAI
+//
+//  Created by Roberto Oregon on 07.03.20.
+//  Copyright © 2020 Roberto Oregon. All rights reserved.
+//
+
+import Foundation
+
+enum ProductError:Error {
+    case noDataAvailable
+    case canNotProcessData
+}
+
+struct ProductRequest {
+    let resourceURL:URL
+    let API_KEY = "3954daf7ed03476db8320da92fe83b61"
+    
+    init(productSearch:String) {
+        
+        let resourceString = "https://api.spoonacular.com/food/products/search?query=\(productSearch)&apiKey=\(API_KEY)"
+        guard let resourceURL = URL(string: resourceString) else {fatalError()}
+        
+        print(resourceString)
+        self.resourceURL = resourceURL
+        
+    }
+    
+    func getProducts (completion: @escaping(Result<ProductResponse, Error>) -> Void) {
+        let dataTask = URLSession.shared.dataTask(with: resourceURL) { data, response, error in
+            if let error = error { completion(.failure(error));  return }
+            
+            
+            do {
+                print("entered do")
+                let productsResponse = try JSONDecoder().decode(ProductResponse.self, from: data!)
+                print("test1")
+                //let productDetails = productsResponse.products
+                print("test2")
+                completion(.success(productsResponse))
+            }catch{
+                completion(.failure(error))
+            }
+        }
+        dataTask.resume()
+    }
+}
---
+++ SlugNutrition/SlugNutrition/ProductsTableViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ProductsTableViewController.swift.swift
+//  SlugNutrition
+//
+//  THIS .SWIFT FILE IS BASED ON THE CODE PROVIDED BY BRIAN ADVENT
+// https://www.youtube.com/watch?v=tdxKIPpPDAI
+//
+//  Created by Roberto Oregon on 07.03.20.
+//  Copyright © 2020 Roberto Oregon. All rights reserved.
+//
+
+import UIKit
+
+class ProductsTableViewController: UITableViewController {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    var listOfProducts = [ProductResponse.ProductDetail]() {
+        didSet {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+                self.navigationItem.title = "\(self.listOfProducts.count) Products found"
+            }
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        searchBar.delegate = self
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listOfProducts.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let product = listOfProducts[indexPath.row]
+        
+        cell.textLabel?.text = product.title
+        cell.detailTextLabel?.text = String(product.id)
+
+        return cell
+    }
+}
+
+extension ProductsTableViewController : UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let searchBarText = searchBar.text else {return}
+        let productRequest = ProductRequest(productSearch: searchBarText)
+        productRequest.getProducts { [weak self] result in
+            switch result {
+            case .failure(let error):
+                print(error)
+            case .success(let products):
+                self?.listOfProducts = products.products
+            }
+        }
+    }
+}
