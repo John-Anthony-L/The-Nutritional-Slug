fd1f62a5b2f21a3e5852c078530ffcc7d98b2c13
roberto oregon <raoregon@ucsc.edu>
2020-07-11 21:42:09 -0700

    added macro calculations, still testing and then will link to daily goals. removed calorie sort for product search view

--- SlugNutrition/SlugNutrition/UserData.swift
+++ SlugNutrition/SlugNutrition/UserData.swift
@@ -14,14 +14,26 @@ struct UserData: Codable {
     var height: Double
     var weight: Double
     var goal: Int
+    var activity: Int
     
-    init(name: String, gender: String, age: Int, height: Double, weight: Double, goal: Int) {
+    var calorieGoal: Double
+    var proteinGoal: Double
+    var fatGoal: Double
+    var carbGoal: Double
+    
+    init(name: String, gender: String, age: Int, height: Double, weight: Double, goal: Int, activity: Int) {
         self.name = name
         self.gender = gender
         self.age = age
         self.height = height
         self.weight = weight
         self.goal = goal
+        self.activity = activity
+        
+        self.calorieGoal = calculateCalories(w: weight, h: height, a: age, u: activity) // need to round to 10
+        self.proteinGoal = calculateProteins(w: weight) // need to round to 5
+        self.fatGoal = calculateFats(c: calorieGoal) // need to round to 5
+        self.carbGoal = calculateCarbs(c: calorieGoal, f: fatGoal, p: proteinGoal) // need to round to 5
     }
     
     static let DocumentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
@@ -43,6 +55,44 @@ struct UserData: Codable {
         
         return try? propertyListDecoder.decode(UserData.self, from: codedData)
     }
+}
+
+
+/*
+func roundToFive(n: Double) -> Double {
+  let f = floor(n)
+  return f + round((n-f) * 20) / 20
+}
+
+func roundToTen(n: Double) -> Double {
+  let f = floor(n)
+  return f + round((n-f) * 10) / 10
+}
+*/
+func calculateCalories(w: Double, h: Double, a: Int, u: Int ) -> Double {
+    //let f = floor(w)
+    let x = 10 * (w/2.2046)
+    let y = 6.25 * (h * 2.54)
+    let z = 5 * a - 161
+    let last = u - 200
+    return (x + y - Double(z)) * Double(last)
+}
+
+func calculateProteins(w: Double ) -> Double {
+    //let f = floor(w)
+    let x = w * 0.9
+    return x
+}
+
+func calculateFats(c: Double ) -> Double {
+    //let f = floor(w)
+    let x = 0.25 * c / 9
+    return x
+}
+
+func calculateCarbs(c: Double, f: Double, p: Double) -> Double {
+    let x = (c - 9 * f - 4 * p) / 4
+    return x
 }
 
 
