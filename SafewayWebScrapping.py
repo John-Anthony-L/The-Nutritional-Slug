9f5fa95e4af83611c65d1d6a8c72860a24b97d0f
John-Anthony-L <56487911+John-Anthony-L@users.noreply.github.com>
2020-07-13 15:16:18 -0700

    Update SafewayWebScrapping.py
    Fixed the output data. Also now the function Get_Data_from_item_page() now returns the product name and its calories

--- SafewayWebScrapping.py
+++ SafewayWebScrapping.py
@@ -21,53 +21,70 @@ def read_link(url):
 def Get_Data_from_item_page(page):
     product_soup = read_link(page)
     product_containers = product_soup.findAll("table", {"class": "tableOfIngredients"})
+    product_name_containers = product_soup.find("div", {"class": "product-heading"})
+    product_name = product_name_containers.h2.text
+    print(product_name)
     if len(product_containers) != 0:
         product_info = product_containers[0]
         # this will get you the number of calories
-        print(product_info.find("td", {"class": "table-ingredients-text"}))
-
+        calories = "calories: " + product_info.find("td", {"class": "table-ingredients-text"}).text
+        print(calories)
     # This will get you all the data for the table, it returns and array
         raw_data = product_info.findAll("td", {"class": "table-ingredients-text"})
 
-    # These are definetly true for the soups, have to check if this table hold for all other food items
-        fats = "fats:" + raw_data[4].text
-        carbs = "carbs: " + raw_data[24].text
-        protien = "protien:" + raw_data[31].text
-
-        f.write(raw_data[4].text.replace("Amount Per serving", "") + "," + raw_data[24].text.replace("Amount Per serving", "") + "," + raw_data[32].text.replace("Amount Per serving", "") + "\n")
-        return fats, carbs, protien
+    # Explain what this does and my process for the index
+        fats = raw_data[4].text
+        if 'Dietary Fiber' in raw_data[24].text:
+            carbs = raw_data[25].text
+        else:
+            carbs = raw_data[24].text
+        if raw_data[30] == '':
+            protien = "protien: 0g"
+        else:
+            protien = raw_data[30].text
+        f.write(fats.replace("Amount Per serving", "") + "," + carbs.replace("Amount Per serving","") + "," + protien.replace("Amount Per serving", "") + "\n")
+        return product_name, calories, fats, carbs, protien
     else:
         return 0
 
+def GetSafewayLink(parital_url_container):
+    try:
+        internal_link = parital_url_container['href']
+        return "https://safeway.com" + internal_link
+    except ValueError:
+        print('Invalid container, it does not have a link, does not have attribute href')
+
+def GetItemsFromPage(url):
+    my_url = 'https://www.safeway.com/shop/aisles/canned-goods-soups/soups-ramen.3132.html?sort=&page=1'
+    page_soup = read_link(url)
+    containers = page_soup.findAll("a", {"class": "product-title"})
+    len(containers)
+    # we can see that there are 33 items per page, so we must continue to increase until we get to 33*n = total items
+
+    safeway_link = []
+    name = []
+    info = []
+    for contain in containers:
+        new_link = GetSafewayLink(contain)
+        safeway_link.append(new_link)
+        data = Get_Data_from_item_page(new_link)
+        if data != 0:
+            info.append(data)
+            name.append(contain.text)
 
 
-my_url = 'https://www.safeway.com/shop/aisles/canned-goods-soups/soups-ramen.3132.html?sort=&page=1'
-page_soup = read_link(my_url)
-containers = page_soup.findAll("a",{"class":"product-title"})
-len(containers)
-#we can see that there are 33 items per page, so we must continue to increase until we get to 33*n = total items
+#Test, new data
+a_whole_new_link = 'https://www.safeway.com/shop/aisles.3132.html'
+a_whole_new_soup = read_link(a_whole_new_link)
+a_whole_new_container = a_whole_new_soup.findAll("ul", {"class": "product-subcats"})
+Product_blacklist = ['Baby', 'Coffee', 'Deli', 'Flowers','Fruits & Vegetables', 'Eggs' ,'Meat & Seafood', 'Beef', 'Laundry', 'Care', 'Pet', 'Wine', 'Beer']
 
-safeway_link = []
-name = []
-info = []
-for contain in containers:
-    new_link = "https://safeway.com" + contain["href"]
-    safeway_link.append(new_link)
-    data = Get_Data_from_item_page(new_link)
-    if data != 0:
-        info.append(data)
-        name.append(contain.text)
-        print(contain.text)
-        print(data)
-
-
-
-# Test, new data
-# a_whole_new_link = 'https://www.safeway.com/shop/aisles.3132.html'
-# a_whole_new_soup = read_link(a_whole_new_link)
-# a_whole_new_container = a_whole_new_soup.findAll("ul", {"class": "product-subcats"})
-#
-# for new_contaner in a_whole_new_container:
-#     Product_genre = new_contaner.a.text
-#     if 'Baby' in new_contaner
-
+for new_container in a_whole_new_container:
+    if new_container.a is not None:
+        Product_genre = new_container.a.text
+        test = any([Product_blacklist_item in Product_genre for Product_blacklist_item in Product_blacklist])
+        if not test:
+            print(Product_genre)
+            link = GetSafewayLink(new_container.a)
+            print(link)
+            GetItemsFromPage(link)
